cb4dd3e9fe5173e16639472e0b46c18021cc2108
Kison <445010480@qq.com>
2017-08-01 03:41:20 +0000

    Finish

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -14,4 +15,40 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * 展示用户信息
+     */
+    public function show($id)
+    {
+        $user = User::findOrfail($id);
+        return view('users.show',compact('user'));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * 添加用户信息
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        session()->flash('success','欢迎，您将在这里开启一段新的旅程');
+        return redirect()->route('user.show',[$user]);
+    }
+
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,15 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    /**
+     * @param string $size
+     * @return string
+     * 获取随机头像
+     */
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
